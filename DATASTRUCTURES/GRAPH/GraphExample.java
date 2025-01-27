1877a3b600e841ebe5d29ebcd9ddec359b5f43ab
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-28 00:45:35 +0400

    add GraphExample.java

---
+++ DATASTRUCTURES/GRAPH/GraphExample.java
@@ -0,0 +1,20 @@
+package DATASTRUCTURES.GRAPH;
+
+public class GraphExample {
+    public static void main(String[] args) {
+        int vertices = 5;
+        Graph graph = new Graph(vertices);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+
+        System.out.println("BFS traversal: ");
+        graph.bfs(0);
+
+        System.out.println("DFS traversal: ");
+        graph.dfs(0);
+    }
+}
