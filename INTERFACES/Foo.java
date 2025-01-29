5aceeac5a8021b0e6af52748fe442d715a4c4b9c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-29 20:58:10 +0400

    update Foo.java

--- INTERFACES/Foo.java
+++ INTERFACES/Foo.java
@@ -3,4 +3,10 @@ package INTERFACES;
 @FunctionalInterface
 public interface Foo {
     String method(String string);
+    default String defaultFoo() {
+        return "default Foo";
+    }
+    default String defaultCommon() {
+        return "default Common";
+    }
 }
