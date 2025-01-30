2bc8880fa15c6423ab7bc055b3b535e8c26408c5
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-30 23:31:53 +0400

    add MyDateUsage.java

---
+++ OOP/DATE/MyDateUsage.java
@@ -0,0 +1,11 @@
+package OOP.DATE;
+
+public class MyDateUsage {
+    public static void main(String[] args) {
+        MyDate date1 = new MyDate(30, 1, 2025);
+        MyDate date2 = new MyDate(1, 2, 2025);
+
+        System.out.println(date1);
+        System.out.println(date2);
+    }
+}
