4e998193c88b48aead45de69fe05dd9fd07896f3
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-31 20:30:35 +0400

    add Car.java

---
+++ OOP/COMPARATOR/Car.java
@@ -0,0 +1,25 @@
+package OOP.COMPARATOR;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+}
