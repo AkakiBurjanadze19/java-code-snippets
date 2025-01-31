d1f4e0a4ffbc7441d3daff92e484ce792fb9c28a
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-31 19:08:27 +0400

    add MyClassUsage.java

---
+++ OOP/INTERFACES/MULTIPLE/MyClassUsage.java
@@ -0,0 +1,10 @@
+package OOP.INTERFACES.MULTIPLE;
+
+public class MyClassUsage {
+    public static void main(String[] args) {
+        MyClass demoClass = new MyClass();
+
+        demoClass.firstInterfaceMethod();
+        demoClass.secondInterfaceMethod();
+    }
+}
