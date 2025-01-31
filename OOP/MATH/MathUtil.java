80b89a293e60aebb22a854087808eea20b68bd8c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-01-31 18:01:50 +0400

    add MathUtil.java

---
+++ OOP/MATH/MathUtil.java
@@ -0,0 +1,26 @@
+package OOP.MATH;
+
+public class MathUtil {
+    public static final double PI = 3.14;
+    public static final double E = 2.7;
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static double div(double a, double b) {
+        return a / b;
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
+    }
+}
